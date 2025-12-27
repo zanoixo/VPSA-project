9e8642d682eff65fb3f494931476ef73d2c38b02
zanoixo <62448782+zanoixo@users.noreply.github.com>
2025-12-27 15:11:35 +0100

    added the skeleton for grpc server functions and implemented a simple create user function

--- client/main.go
+++ client/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"time"
 
 	db "github.com/zanoixo/VPSA-project/razpravljalnica"
 	"google.golang.org/grpc"
@@ -28,81 +29,90 @@ type Client struct {
 	id             int64
 }
 
-func (client *Client) CreateUser(name string) *db.User {
+func (client *Client) CreateUser(name string) (*db.User, error) {
 
 	createUsrReq := &db.CreateUserRequest{Name: name}
 
 	createUsrResp, err := client.msgBoardClient.CreateUser(context.Background(), createUsrReq)
+	fmt.Printf("[INFO]: sent a create user request\n")
 	checkError(err)
+	fmt.Printf("[INFO]: recived user ID request %d\n", createUsrResp.Id)
 
-	return createUsrResp
+	return createUsrResp, nil
 
 }
 
-func (client *Client) CreateTopic(name string) *db.Topic {
+func (client *Client) CreateTopic(name string) (*db.Topic, error) {
 
 	newTopicReq := &db.CreateTopicRequest{Name: name}
 
 	createTopicResp, err := client.msgBoardClient.CreateTopic(context.Background(), newTopicReq)
+	fmt.Printf("[INFO] sent a create topic request\n")
 	checkError(err)
 
-	return createTopicResp
+	return createTopicResp, nil
 }
 
-func (client *Client) PostMessage(topicID, userID int64, text string) *db.Message {
+func (client *Client) PostMessage(topicID, userID int64, text string) (*db.Message, error) {
 
 	newPostReq := &db.PostMessageRequest{TopicId: topicID, UserId: userID, Text: text}
 
 	PostResp, err := client.msgBoardClient.PostMessage(context.Background(), newPostReq)
+	fmt.Printf("[INFO] sent a post message request\n")
 	checkError(err)
 
-	return PostResp
+	return PostResp, nil
 }
 
-func (client *Client) LikeMessage(topicID, messageID, userID int64) *db.Message {
+func (client *Client) LikeMessage(topicID, messageID, userID int64) (*db.Message, error) {
 
 	newLikeReq := &db.LikeMessageRequest{TopicId: topicID, MessageId: messageID, UserId: userID}
 
 	LikeResp, err := client.msgBoardClient.LikeMessage(context.Background(), newLikeReq)
+	fmt.Printf("[INFO] sent a like message request\n")
 	checkError(err)
 
-	return LikeResp
+	return LikeResp, nil
 }
 
-func (client *Client) GetSubscriptionNode(userID int64, topicIDs []int64) *db.SubscriptionNodeResponse {
+func (client *Client) GetSubscriptionNode(userID int64, topicIDs []int64) (*db.SubscriptionNodeResponse, error) {
 
 	subNodeReq := &db.SubscriptionNodeRequest{UserId: userID, TopicId: topicIDs}
 
 	SubNodeResp, err := client.msgBoardClient.GetSubscriptionNode(context.Background(), subNodeReq)
+	fmt.Printf("[INFO] sent a get subscription node request\n")
 	checkError(err)
 
-	return SubNodeResp
+	return SubNodeResp, nil
 }
 
-func (client *Client) ListTopics() *db.ListTopicsResponse {
+func (client *Client) ListTopics() (*db.ListTopicsResponse, error) {
 
 	listTopicsReq := &emptypb.Empty{}
 
 	listTopicsRes, err := client.msgBoardClient.ListTopics(context.Background(), listTopicsReq)
+	fmt.Printf("[INFO] sent a list topics request\n")
 	checkError(err)
 
-	return listTopicsRes
+	return listTopicsRes, nil
 }
 
-func (client *Client) GetMessages(topicID int64, fromMessageID int64, limit int32) *db.GetMessagesResponse {
+func (client *Client) GetMessages(topicID int64, fromMessageID int64, limit int32) (*db.GetMessagesResponse, error) {
 
 	getMsgReq := &db.GetMessagesRequest{TopicId: topicID, FromMessageId: fromMessageID, Limit: limit}
 
 	msgResp, err := client.msgBoardClient.GetMessages(context.Background(), getMsgReq)
+	fmt.Printf("[INFO] sent a get messages request\n")
 	checkError(err)
 
-	return msgResp
+	return msgResp, nil
 }
 
-func (client *Client) recvTopicEvents(msgEvents chan *db.MessageEvent, req *db.SubscribeTopicRequest) {
+func (client *Client) recvTopicEvents(msgEvents chan *db.MessageEvent, req *db.SubscribeTopicRequest) error {
 	defer close(msgEvents)
 
 	msgStream, err := client.msgBoardClient.SubscribeTopic(context.Background(), req)
+	fmt.Printf("[INFO] sent a subscribe topic request\n")
 	checkError(err)
 
 	for {
@@ -110,7 +120,7 @@ func (client *Client) recvTopicEvents(msgEvents chan *db.MessageEvent, req *db.S
 		newMsg, err := msgStream.Recv()
 
 		if err == io.EOF {
-			return
+			return nil
 		}
 		checkError(err)
 
@@ -119,7 +129,7 @@ func (client *Client) recvTopicEvents(msgEvents chan *db.MessageEvent, req *db.S
 
 }
 
-func (client *Client) SubscribeTopic(topicIDs []int64, userID, fromMessageID int64, token string) <-chan *db.MessageEvent {
+func (client *Client) SubscribeTopic(topicIDs []int64, userID, fromMessageID int64, token string) (<-chan *db.MessageEvent, error) {
 
 	msgEvents := make(chan *db.MessageEvent)
 
@@ -127,17 +137,18 @@ func (client *Client) SubscribeTopic(topicIDs []int64, userID, fromMessageID int
 
 	go client.recvTopicEvents(msgEvents, subTopicReq)
 
-	return msgEvents
+	return msgEvents, nil
 }
 
-func (client *Client) GetClusterState() *db.GetClusterStateResponse {
+func (client *Client) GetClusterState() (*db.GetClusterStateResponse, error) {
 
 	clusterStateReq := &emptypb.Empty{}
 
 	clusterStateRes, err := client.controlClient.GetClusterState(context.Background(), clusterStateReq)
+	fmt.Printf("[INFO] sent a get cluster state request\n")
 	checkError(err)
 
-	return clusterStateRes
+	return clusterStateRes, nil
 }
 
 func startClient(url string, name string) error {
@@ -152,6 +163,10 @@ func startClient(url string, name string) error {
 	client.msgBoardClient = db.NewMessageBoardClient(conn)
 	client.name = name
 	client.CreateUser(name)
+
+	for {
+		time.Sleep(time.Second)
+	}
 
 	return nil
 }
--- worker-server/main.go
+++ worker-server/main.go
@@ -1,9 +1,87 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"net"
+	"sync"
+
+	db "github.com/zanoixo/VPSA-project/razpravljalnica"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
+
+type Server struct {
+	db.UnimplementedMessageBoardServer
+	CRUDServer *ServerDataBase
+	dbLock     sync.Mutex
+}
+
+type ServerDataBase struct {
+	users     map[string]int64
+	userIndex int64
+}
+
+func (server *Server) CreateUser(ctx context.Context, req *db.CreateUserRequest) (*db.User, error) {
+
+	fmt.Printf("[INFO]: Recieved create user request from %s\n", req.Name)
+
+	currUser := db.User{}
+	currUser.Name = req.Name
+
+	userId, userExists := server.CRUDServer.users[req.Name]
+
+	if userExists {
+
+		fmt.Printf("[INFO]: User exists returning user ID %d\n", userId)
+		currUser.Id = userId
+	} else {
+
+		fmt.Printf("[INFO]: Creating user with ID %d\n", server.CRUDServer.userIndex)
+
+		server.dbLock.Lock()
+
+		server.CRUDServer.users[req.Name] = server.CRUDServer.userIndex
+		currUser.Id = server.CRUDServer.userIndex
+		server.CRUDServer.userIndex++
+
+		server.dbLock.Unlock()
+
+	}
+
+	return &currUser, nil
+}
+
+func (server *Server) CreateTopic(ctx context.Context, req *db.CreateTopicRequest) (*db.Topic, error) {
+	return nil, status.Error(codes.Unimplemented, "method CreateTopic not implemented")
+}
+
+func (server *Server) PostMessage(ctx context.Context, req *db.PostMessageRequest) (*db.Message, error) {
+	return nil, status.Error(codes.Unimplemented, "method PostMessage not implemented")
+}
+
+func (server *Server) LikeMessage(ctx context.Context, req *db.LikeMessageRequest) (*db.Message, error) {
+	return nil, status.Error(codes.Unimplemented, "method LikeMessage not implemented")
+}
+
+func (server *Server) GetSubscriptionNode(ctx context.Context, req *db.SubscriptionNodeRequest) (*db.SubscriptionNodeResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "method GetSubscriptionNode not implemented")
+}
+
+func (server *Server) ListTopics(ctx context.Context, req *emptypb.Empty) (*db.ListTopicsResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "method ListTopics not implemented")
+}
+
+func (server *Server) GetMessages(ctx context.Context, req *db.GetMessagesRequest) (*db.GetMessagesResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "method GetMessages not implemented")
+}
+
+func (server *Server) SubscribeTopic(req *db.SubscribeTopicRequest, stream db.MessageBoard_SubscribeTopicServer) error {
+	return status.Error(codes.Unimplemented, "method SubscribeTopic not implemented")
+}
 
 func checkError(err error) {
 
@@ -13,10 +91,26 @@ func checkError(err error) {
 
 }
 
-func startServer(url string) error {
+func startServer(url string) {
+
+	CRUD := ServerDataBase{}
+	CRUD.users = make(map[string]int64)
+	CRUD.userIndex = 0
 
 	fmt.Printf("Server starting: %s\n", url)
-	return nil
+	grpcServer := grpc.NewServer()
+
+	server := Server{}
+	server.CRUDServer = &CRUD
+
+	db.RegisterMessageBoardServer(grpcServer, &server)
+
+	listener, err := net.Listen("tcp", url)
+	checkError(err)
+
+	err = grpcServer.Serve(listener)
+	checkError(err)
+
 }
 
 func main() {
